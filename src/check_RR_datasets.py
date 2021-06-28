b594f8bfd8e4274deda85f0880da238a5a50c0db
kangzhiq <709563092@qq.com>
2021-06-28 14:00:28 +0200

    check nb frames

--- src/check_RR_datasets.py
+++ src/check_RR_datasets.py
@@ -13,8 +13,11 @@ for id in os.listdir(root_frontal_dir):
     id_masks_dir = os.path.join(root_masks_dir, id)
     frontal_lst = os.listdir(id_frontal_dir)
     masks_lst = os.listdir(id_masks_dir)
-    print(id_frontal_dir, ': ', len(frontal_lst))
-    print(id_masks_dir, ': ', len(masks_lst))
+
+    if len(frontal_lst) < 150 or len(masks_lst) <150:
+        os.rmdir(id_frontal_dir)
+        os.rmdir(id_masks_dir)
+
     if frontal_lst == [] or masks_lst == []:
         os.rmdir(id_frontal_dir)
         os.rmdir(id_masks_dir)
